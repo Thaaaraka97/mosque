9964279b5c5d2d747fccb384994be8e04dc07ce7
Thaaaraka97 <tharakadeepakara123@gmail.com>
2020-12-20 21:46:13 +0530

    initial commit

---
+++ admin/forms.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . "/mosque/admin/template_parts/header.php";
+?>
+
+<body>
+  <div class="container-scroller">
+    <!-- navigation bar -->
+    <?php
+
+    include $_SERVER['DOCUMENT_ROOT'] . "/mosque/admin/template_parts/navbar.php";
+    ?>
+
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- top bar -->
+      <?php
+
+      include $_SERVER['DOCUMENT_ROOT'] . "/mosque/admin/template_parts/topbar.php";
+      ?>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="page-header">
+            <h3 class="page-title"> Forms </h3>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Forms</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Form elements</li>
+              </ol>
+            </nav>
+          </div>
+          <div class="row">
+            <!-- forms body -->
+            <div class="col-md-3 grid-margin">
+              <div class="card text-center text-dark" style="width: 18rem;">
+                <div class="card-body">
+                <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+
+                  <h5 class="card-title mt-3">Special title treatment</h5>
+                  <a href="#" class="btn btn-primary btn-lg">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 grid-margin">
+              <div class="card text-center text-dark" style="width: 18rem;">
+                <div class="card-body">
+                <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+
+                  <h5 class="card-title mt-3">Special title treatment</h5>
+                  <a href="#" class="btn btn-primary btn-lg">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 grid-margin">
+              <div class="card text-center text-dark" style="width: 18rem;">
+                <div class="card-body">
+                <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+
+                  <h5 class="card-title mt-3">Special title treatment</h5>
+                  <a href="#" class="btn btn-primary btn-lg">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 grid-margin">
+              <div class="card text-center text-dark" style="width: 18rem;">
+                <div class="card-body">
+                <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+
+                  <h5 class="card-title mt-3">Special title treatment</h5>
+                  <a href="#" class="btn btn-primary btn-lg">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+
+            
+          </div>
+          <div class="row">
+            <!-- forms body -->
+            <div class="col-md-3 grid-margin">
+              <div class="card text-center text-dark" style="width: 18rem;">
+                <div class="card-body">
+                <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+
+                  <h5 class="card-title mt-3">Special title treatment</h5>
+                  <a href="#" class="btn btn-primary btn-lg">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 grid-margin">
+              <div class="card text-center text-dark" style="width: 18rem;">
+                <div class="card-body">
+                <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+
+                  <h5 class="card-title mt-3">Special title treatment</h5>
+                  <a href="#" class="btn btn-primary btn-lg">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 grid-margin">
+              <div class="card text-center text-dark" style="width: 18rem;">
+                <div class="card-body">
+                <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+
+                  <h5 class="card-title mt-3">Special title treatment</h5>
+                  <a href="#" class="btn btn-primary btn-lg">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 grid-margin">
+              <div class="card text-center text-dark" style="width: 18rem;">
+                <div class="card-body">
+                <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+
+                  <h5 class="card-title mt-3">Special title treatment</h5>
+                  <a href="#" class="btn btn-primary btn-lg">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+
+            
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- footer -->
+  <?php
+
+  include "$server_name/mosque/admin/template_parts/footer.php";
+  ?>
+  <!-- End custom js for this page -->
+</body>
+
+</html>
---
+++ admin/include/include.php
@@ -0,0 +1,4 @@
+<!-- extract the server name -->
+<?php
+$server_name = $_SERVER['DOCUMENT_ROOT'];
+?>
