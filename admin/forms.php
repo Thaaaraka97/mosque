56aa6e77ee7b2cbbf37b2c190414553016eb72ee
Thaaaraka97 <tharakadeepakara123@gmail.com>
2021-01-01 20:29:15 +0530

    forms.php changed

--- admin/forms.php
+++ admin/forms.php
@@ -32,17 +32,17 @@ include "template_parts/header.php";
             <div class="col-md-3 grid-margin">
               <div class="card d-flex alig-content-end text-center text-dark shadow-sm" style="height : 100%;">
                 <div class="card-body">
-                  <img src="<?php echo $server_name ?>admin/assets/images/icons/group-meeting.png" alt="Trustee Board">
+                  <img src="<?php echo $server_name ?>assets/images/icons/group-meeting.png" alt="Trustee Board">
 
                   <h5 class="card-title mt-3">Trustee Board Details</h5>
-                  <a href="<?php echo $server_name ?>admin/trustee-board-form.php" class="btn btn-primary py-3 mt-3">Open..</a>
+                  <a href="<?php echo $server_name ?>trustee-board-form.php" class="btn btn-primary py-3 mt-3">Open..</a>
                 </div>
               </div>
             </div>
             <div class="col-md-3 grid-margin flip-card">
               <div class="text-center text-dark shadow-sm flip-card-inner" style="height : 100%;">
                 <div class="card-body flip-card-front">
-                  <img class="pt-4" src="<?php echo $server_name ?>admin/assets/images/icons/shop.png" alt="Trustee Board">
+                  <img class="pt-4" src="<?php echo $server_name ?>assets/images/icons/shop.png" alt="Trustee Board">
 
                   <h5 class="card-title mt-5">Rental Details</h5>
                 </div>
@@ -52,10 +52,10 @@ include "template_parts/header.php";
                       Click to open..
                     </p>
                     <p>
-                      <a href="<?php echo $server_name ?>admin/add-new-rental.php" class="btn btn-primary btn-md">Add new Rental</a>
+                      <a href="<?php echo $server_name ?>add-new-rental.php" class="btn btn-primary btn-md">Add new Rental</a>
                     </p>
                     <p>
-                      <a href="<?php echo $server_name ?>admin/add-payment.php" class="btn btn-primary btn-md">Add a payment</a>
+                      <a href="<?php echo $server_name ?>add-payment.php" class="btn btn-primary btn-md">Add a payment</a>
                     </p>
                   </div>
                 </div>
@@ -64,20 +64,20 @@ include "template_parts/header.php";
             <div class="col-md-3 grid-margin">
               <div class="card text-center text-dark shadow-sm" style="height : 100%;">
                 <div class="card-body">
-                  <img src="<?php echo $server_name ?>admin/assets/images/icons/quran.png" alt="Trustee Board">
+                  <img src="<?php echo $server_name ?>assets/images/icons/quran.png" alt="Trustee Board">
 
                   <h5 class="card-title mt-3">Quran Madhrasa Registration Details</h5>
-                  <a href="<?php echo $server_name ?>admin/quran-registration-form.php" class="btn btn-primary py-3">Open..</a>
+                  <a href="<?php echo $server_name ?>quran-registration-form.php" class="btn btn-primary py-3">Open..</a>
                 </div>
               </div>
             </div>
             <div class="col-md-3 grid-margin">
               <div class="card text-center text-dark shadow-sm" style="height : 100%;">
                 <div class="card-body">
-                  <img src="<?php echo $server_name ?>admin/assets/images/icons/wedding-couple.png" alt="Trustee Board">
+                  <img src="<?php echo $server_name ?>assets/images/icons/wedding-couple.png" alt="Trustee Board">
 
                   <h5 class="card-title mt-3">Nikkah Details</h5>
-                  <a href="<?php echo $server_name ?>admin/nikkah-details-form.php" class="btn btn-primary py-3 mt-3">Open..</a>
+                  <a href="<?php echo $server_name ?>nikkah-details-form.php" class="btn btn-primary py-3 mt-3">Open..</a>
                 </div>
               </div>
             </div>
@@ -89,27 +89,27 @@ include "template_parts/header.php";
             <div class="col-md-3 grid-margin">
               <div class="card text-center text-dark shadow-sm" style="height : 100%;">
                 <div class="card-body">
-                  <img src="<?php echo $server_name ?>admin/assets/images/icons/moon.png" alt="Trustee Board">
+                  <img src="<?php echo $server_name ?>assets/images/icons/moon.png" alt="Trustee Board">
 
                   <h5 class="card-title mt-3">Janaza Details</h5>
-                  <a href="<?php echo $server_name ?>admin/janaza-details-form.php" class="btn btn-primary py-3">Open..</a>
+                  <a href="<?php echo $server_name ?>janaza-details-form.php" class="btn btn-primary py-3">Open..</a>
                 </div>
               </div>
             </div>
             <div class="col-md-3 grid-margin">
               <div class="card text-center text-dark shadow-sm" style="height : 100%;">
                 <div class="card-body">
-                  <img src="<?php echo $server_name ?>admin/assets/images/icons/member.png" alt="Member">
+                  <img src="<?php echo $server_name ?>assets/images/icons/member.png" alt="Member">
 
                   <h5 class="card-title mt-3">Saandha Registration Details</h5>
-                  <a href="<?php echo $server_name ?>admin/saandha-registration-form.php" class="btn btn-primary py-3">Open..</a>
+                  <a href="<?php echo $server_name ?>saandha-registration-form.php" class="btn btn-primary py-3">Open..</a>
                 </div>
               </div>
             </div>
             <div class="col-md-3 grid-margin flip-card">
               <div class="text-center text-dark shadow-sm flip-card-inner" style="height : 100%;">
                 <div class="card-body flip-card-front">
-                  <img class="pt-4" src="<?php echo $server_name ?>admin/assets/images/icons/online-donation.png" alt="Trustee Board">
+                  <img class="pt-4" src="<?php echo $server_name ?>assets/images/icons/online-donation.png" alt="Trustee Board">
 
                   <h5 class="card-title mt-5">Donations</h5>
                 </div>
@@ -119,10 +119,10 @@ include "template_parts/header.php";
                       Click to open..
                     </p>
                     <p>
-                      <a href="<?php echo $server_name ?>admin/disaster-relief-donations.php" class="btn btn-primary btn-md">Disaster Relief Donations</a>
+                      <a href="<?php echo $server_name ?>disaster-relief-donations.php" class="btn btn-primary btn-md">Disaster Relief Donations</a>
                     </p>
                     <p>
-                      <a href="<?php echo $server_name ?>admin/other-donations.php" class="btn btn-primary btn-md">Other Donations</a>
+                      <a href="<?php echo $server_name ?>other-donations.php" class="btn btn-primary btn-md">Other Donations</a>
                     </p>
                   </div>
 
@@ -133,7 +133,7 @@ include "template_parts/header.php";
             <div class="col-md-3 grid-margin flip-card">
               <div class="text-center text-dark shadow-sm flip-card-inner" style="height : 100%;">
                 <div class="card-body flip-card-front">
-                  <img class="pt-4" src="<?php echo $server_name ?>admin/assets/images/icons/muslim.png" alt="Trustee Board">
+                  <img class="pt-4" src="<?php echo $server_name ?>assets/images/icons/muslim.png" alt="Trustee Board">
 
                   <h5 class="card-title mt-5">Head Priest</h5>
                 </div>
@@ -143,10 +143,10 @@ include "template_parts/header.php";
                       Click to open..
                     </p>
                     <p>
-                      <a href="<?php echo $server_name ?>admin/pesh-imaam-details.php" class="btn btn-primary btn-md">Pesh Imaam Details</a>
+                      <a href="<?php echo $server_name ?>pesh-imaam-details.php" class="btn btn-primary btn-md">Pesh Imaam Details</a>
                     </p>
                     <p>
-                      <a href="<?php echo $server_name ?>admin/muazzin-details.php" class="btn btn-primary btn-md">Muazzin Details</a>
+                      <a href="<?php echo $server_name ?>muazzin-details.php" class="btn btn-primary btn-md">Muazzin Details</a>
                     </p>
                   </div>
                 </div>
