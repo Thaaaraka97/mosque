c3800bb94f9764af0f44379506ab42aff4236375
Thaaaraka97 <tharakadeepakara123@gmail.com>
2021-01-01 16:28:03 +0530

    template_parts untracked; global link added for links

--- admin/include/include.php
+++ admin/include/include.php
@@ -1,4 +1,23 @@
 <!-- extract the server name -->
 <?php
-$server_name = $_SERVER['DOCUMENT_ROOT'];
+
+if(siteURL() == 'http://localhost/') {
+    $server_name = siteURL()."mosque/";
+}
+else if(siteURL() == 'http://annoorjummamusjid1.lk/'){
+    $server_name = siteURL();
+}
+else {
+    return false;
+}
+
+function siteURL()
+{
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $domainName = $_SERVER['HTTP_HOST'].'/';
+    return $protocol.$domainName;
+}
+
+echo $server_name;
 ?>
+
---
+++ admin/template_parts/header.php
@@ -0,0 +1,44 @@
+<?php
+if (siteURL() == 'http://localhost/') {
+    $server_name = siteURL() . "mosque/";
+} else if (siteURL() == 'http://annoorjummamusjid1.lk/') {
+    $server_name = siteURL();
+} else {
+    return false;
+}
+
+function siteURL()
+{
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $domainName = $_SERVER['HTTP_HOST'] . '/';
+    return $protocol . $domainName;
+}
+
+// echo $server_name;
+?>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>An-Noor Jumma Masjid</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="<?php $server_name ?>admin/assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="<?php $server_name ?>admin/assets/vendors/css/vendor.bundle.base.css" ; ?>>
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="<?php $server_name ?>admin/assets/vendors/jvectormap/jquery-jvectormap.css">
+    <link rel="stylesheet" href="<?php $server_name ?>admin/assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="<?php $server_name ?>admin/assets/vendors/owl-carousel-2/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php $server_name ?>admin/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="<?php $server_name ?>admin/assets/css/style.css">
+    <link rel="stylesheet" href="<?php $server_name ?>admin/assets/css/light-version.css">
+    <link rel="stylesheet" href="<?php $server_name ?>admin/assets/css/custom-styles.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="<?php $server_name ?>admin/assets/images/logo-mini.png" />
+</head>
